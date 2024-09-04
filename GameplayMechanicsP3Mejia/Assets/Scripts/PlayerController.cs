8a35514357be8b1895a6f3e4544a9b0ba84d934c
Allan Mejia-Mora <723495@student.dusd.net>
2024-09-04 11:45:36 -0700

    DoneRockets&smash

--- GameplayMechanicsP3Mejia/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsP3Mejia/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,18 @@ public class PlayerController : MonoBehaviour
     public bool hasPowerup;
     private float powerupStrength = 15.0f;
     public GameObject powerupIndicater;
+    public PowerUpType currentPowerUp = PowerUpType.None;
+
+    public GameObject rocketPrefab; 
+    private GameObject tmpRocket; 
+    private Coroutine powerupCountdown;
+
+    public float hangTime; 
+    public float smashSpeed; 
+    public float explosionForce; 
+    public float explosionRadius; 
+    bool smashing = false; 
+    float floorY;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +35,23 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             Destroy(other.gameObject);
+
+            if (powerupCountdown != null) 
+            { 
+                StopCoroutine(powerupCountdown);
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
+
+
             StartCoroutine(PowerupCountdownRoutine());
             powerupIndicater.gameObject.SetActive(true);
             IEnumerator PowerupCountdownRoutine()
             {
                 yield return new WaitForSeconds(7);
                 hasPowerup = false;
+                currentPowerUp = PowerUpType.None;
                 powerupIndicater.gameObject.SetActive(false);
             }
         }
@@ -41,18 +63,66 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         powerupIndicater.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F)) 
+        {
+            LaunchRockets();
+        }
+
+        if (currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true; StartCoroutine(Smash());
+        }
     }
     
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Pushback)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
 
             Debug.Log("Collided with" + collision.gameObject.name + " with powerup set to " + hasPowerup);
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + currentPowerUp.ToString());
         }
        
+    }
+
+
+    void LaunchRockets() 
+    {
+        foreach (var enemy in FindObjectsOfType<Enemy>())
+        {
+            tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
+            tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
+        } 
+    }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+
+        floorY = transform.position.y;
+
+        float jumpTime = Time.time + hangTime;
+
+        while (Time.time < jumpTime)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+                    transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+        smashing = false;
     }
 }
--- GameplayMechanicsP3Mejia/Assets/Scripts/SpawnManger.cs
+++ GameplayMechanicsP3Mejia/Assets/Scripts/SpawnManger.cs
@@ -9,13 +9,14 @@ public class SpawnManger : MonoBehaviour
     private float spawnRange = 9;
     public int enemyCount;
     public int waveNumber = 1;
-    public GameObject powerupPrefab;
+    public GameObject[] powerupPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+        Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
         SpawnEnemyWave(waveNumber);
-        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        
       
     }
 
@@ -45,7 +46,8 @@ public class SpawnManger : MonoBehaviour
         { 
             waveNumber ++;
             SpawnEnemyWave(waveNumber);
-            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+            int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+            Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
         }
 
         
