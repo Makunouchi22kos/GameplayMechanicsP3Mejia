f78e3e9947134a61df832f01bd941d25ed965273
Allan Mejia-Mora <723495@student.dusd.net>
2024-09-16 11:18:32 -0700

    DOne

--- GameplayMechanicsP3Mejia/Assets/Scripts/Enemy.cs
+++ GameplayMechanicsP3Mejia/Assets/Scripts/Enemy.cs
@@ -7,12 +7,21 @@ public class Enemy : MonoBehaviour
     public float speed; 
     private Rigidbody enemyRb;
     private GameObject player;
+    public bool isBoss = false;
+    public float spawnInterval;
+    private float nextSpawn;
+    public int miniEnemySpawnCount; 
+    private SpawnManager spawnManager;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+        if (isBoss) 
+        { 
+            spawnManager = FindObjectOfType<SpawnManager>(); 
+        }
     }
 
     // Update is called once per frame
@@ -22,10 +31,20 @@ public class Enemy : MonoBehaviour
 
         enemyRb.AddForce(lookDirection * speed);
 
-        if(transform.position.y < -10)
+        if (isBoss) 
+        { 
+            if (Time.time > nextSpawn) 
+            {
+                nextSpawn = Time.time + spawnInterval; spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+            } 
+        }
+
+        if (transform.position.y < -10)
         {
             Destroy(gameObject);
         }
+
+
 
             
     }
--- GameplayMechanicsP3Mejia/Assets/Scripts/SpawnManger.cs
+++ GameplayMechanicsP3Mejia/Assets/Scripts/SpawnManger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManger : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public GameObject bigEnemyPrefab;
@@ -10,6 +10,9 @@ public class SpawnManger : MonoBehaviour
     public int enemyCount;
     public int waveNumber = 1;
     public GameObject[] powerupPrefabs;
+    public GameObject bossPrefab;
+    public GameObject[] miniEnemyPrefabs;
+    public int bossRound;
     // Start is called before the first frame update
     void Start()
     {
@@ -45,11 +48,50 @@ public class SpawnManger : MonoBehaviour
         if (enemyCount == 0)
         { 
             waveNumber ++;
-            SpawnEnemyWave(waveNumber);
+            if (waveNumber % bossRound == 0) 
+            { 
+                SpawnBossWave(waveNumber);
+            } 
+            else 
+            { 
+                SpawnEnemyWave(waveNumber);
+            }
+
             int randomPowerup = Random.Range(0, powerupPrefabs.Length);
             Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(), powerupPrefabs[randomPowerup].transform.rotation);
         }
 
         
     }
+
+    void SpawnBossWave(int currentRound)
+    {
+        int miniEnemysToSpawn;  
+        if (bossRound != 0)
+        { 
+            miniEnemysToSpawn = currentRound / bossRound; 
+        } 
+        else
+        { 
+            miniEnemysToSpawn = 1; 
+        } 
+
+        var boss = Instantiate(bossPrefab, GenerateSpawnPosition(),
+            bossPrefab.transform.rotation);
+        boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemysToSpawn;
+    }
+
+
+    public void SpawnMiniEnemy(int amount)
+    { 
+        for (int i = 0; i < amount; i++) 
+        { 
+            int randomMini = Random.Range(0, miniEnemyPrefabs.Length);
+            Instantiate(miniEnemyPrefabs[randomMini], GenerateSpawnPosition(),
+                miniEnemyPrefabs[randomMini].transform.rotation);
+        }
+    }
+
+
+
 }
